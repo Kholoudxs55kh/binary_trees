bde2ad155e93c60e7aefa0fa7b6eff8a5b538764
Kholoudxs55kh <kfattem@gmail.com>
2023-08-30 14:12:52 -0700

    Fixing 6&7&9 by some help, To Relook

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,7 +10,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t HLeft;
 	size_t HRight;
 
-	HLeft = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-	HRight = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return (HLeft > HRight ? HLeft : HRight);
+	if (!tree)
+		return (0);
+
+	else
+	{
+		if (tree)
+		{
+			HLeft = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+			HRight = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		}
+		return (HLeft > HRight ? HLeft : HRight);
+	}
 }

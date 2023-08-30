fe4878e0f0438bfdfc9578002ca278d61c9a5d4f
Kholoudxs55kh <kfattem@gmail.com>
2023-08-30 14:00:28 -0700

    fixing betty but cant get the error with 6th task

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_preorder -  goes through a binary tree using pre-order traversal
  * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node. The value in the node must be passed
+ * @func: pointer to a func to call for each node.value in node must be passed
  * Return: Void
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_postorder - goes through a binary tree using post-order traversal
+ * binary_tree_postorder - go through binarytree using post-order traversal
  * @tree: pointer to the root node of the tree to traverse
- * @func: pointer to a function to call for each node. The value in the node must be passed
+ * @func: pointer to func to call each node. The value in node must be passed
  * Return: Void
  */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
